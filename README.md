23b8b1fe5c3eefed81e3af13cc91e1bd7a63380f
Jen Wilhelm <jen.wilhelm@gmail.com>
2015-12-15 12:23:22 -0800

    Demo for VegasJS talk.

---
+++ README.md
@@ -0,0 +1,3 @@
+## Slides from the talk can be found at:
+
+[https://docs.google.com/presentation/d/1JAnGBhtIdaw2IJCg0YrBpJwUzOONK86gv1MtyEM5SpY/edit?usp=sharing](https://docs.google.com/presentation/d/1JAnGBhtIdaw2IJCg0YrBpJwUzOONK86gv1MtyEM5SpY/edit?usp=sharing)
---
+++ js/jquery.konami.js
@@ -0,0 +1,22 @@
+;(function( $ ) {
+
+	$.fn.konami = function(options) {
+
+		// Default options
+    var settings = $.extend({ }, options );
+
+    var keys = [];
+    var konami = '38,38,40,40,37,39,37,39,66,65';
+
+    $(window).keydown(function(e) {
+    	keys.push( e.keyCode );
+    	if ( keys.toString().indexOf( konami ) >= 0  ) {
+    		// Konami code! Do something cool
+    		$('#konami').html('<iframe width="420" height="315" src="https://www.youtube.com/embed/iJ4T9CQA0UM?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>');
+    	}
+    });
+
+	  return this;
+	};
+
+}( jQuery ));
